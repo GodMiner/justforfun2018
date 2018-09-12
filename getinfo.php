cdc96972835361eacc74d5dd4073b268357fe1b6
GodMiner <God_is_girl@outlook.com>
2018-09-12 01:43:07 -0700

    Create getinfo.php

---
+++ getinfo.php
@@ -0,0 +1,70 @@
+<?php
+ error_reporting(E_ALL);
+include 'medoo.php';
+
+
+// Using Medoo namespace
+use Medoo\Medoo;
+ 
+$database = new Medoo([
+	// required
+	'database_type' => 'mysql',
+	'database_name' => 'phonedata',
+	'server' => '127.0.0.1',
+	'username' => 'root',
+	'password' => 'root',
+ 
+	// [optional]
+	'charset' => 'utf8',
+	'port' => 3306,
+ 
+	// [optional] Table prefix
+	'prefix' => '',
+ 
+	// [optional] Enable logging (Logging is disabled by default for better performance)
+	'logging' => true,
+ 
+	// [optional] MySQL socket (shouldn't be used with server and port)
+	'socket' => '/tmp/mysql.sock',
+ 
+	// [optional] driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+	'option' => [
+		PDO::ATTR_CASE => PDO::CASE_NATURAL
+	],
+ 
+	// [optional] Medoo will execute those commands after connected to the database for initialization
+	'command' => [
+		'SET SQL_MODE=ANSI_QUOTES'
+	]
+]);
+ 
+// $database->insert("account", [
+// 	"user_name" => "foo",
+// 	"email" => "foo@bar.com"
+// ]);
+
+// $database->insert("datatest", [
+// 	"Name" => "xxxxxx",
+//     "Money" => "$10000",
+//     "payment_method" => "中國銀行",
+//     "Time" => "2222 2-22 22：22",
+//     "User" => "二貨" 
+// ]);
+
+
+$datas = $database->select("datatest",
+["id","Name", "Money", "payment_method", "Time", "User"]
+);
+
+#var_dump($datas);
+
+echo(json_encode($datas));
+
+
+// foreach($datas as $data)
+// {
+//     echo $data["Name"] ;
+//     echo $data["Money"];
+// }
+
+?>
